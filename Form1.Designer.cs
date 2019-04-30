a69c77835e30451a7b609b53befaabc6ea51e289
Michael Murray <num1stratplayer@yahoo.com>
2019-04-30 13:40:02 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AutoRestart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileLoc = new System.Windows.Forms.TextBox();
+            this.btnGetFile = new System.Windows.Forms.Button();
+            this.btnAddToMonitorList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.chkAutoStart = new System.Windows.Forms.CheckBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Program to restart";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // txtFileLoc
+            // 
+            this.txtFileLoc.Location = new System.Drawing.Point(187, 12);
+            this.txtFileLoc.Name = "txtFileLoc";
+            this.txtFileLoc.Size = new System.Drawing.Size(338, 20);
+            this.txtFileLoc.TabIndex = 1;
+            // 
+            // btnGetFile
+            // 
+            this.btnGetFile.Location = new System.Drawing.Point(106, 10);
+            this.btnGetFile.Name = "btnGetFile";
+            this.btnGetFile.Size = new System.Drawing.Size(75, 23);
+            this.btnGetFile.TabIndex = 2;
+            this.btnGetFile.Text = "File";
+            this.btnGetFile.UseVisualStyleBackColor = true;
+            this.btnGetFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAddToMonitorList
+            // 
+            this.btnAddToMonitorList.Location = new System.Drawing.Point(15, 39);
+            this.btnAddToMonitorList.Name = "btnAddToMonitorList";
+            this.btnAddToMonitorList.Size = new System.Drawing.Size(510, 23);
+            this.btnAddToMonitorList.TabIndex = 3;
+            this.btnAddToMonitorList.Text = "Add to Monitor List";
+            this.btnAddToMonitorList.UseVisualStyleBackColor = true;
+            this.btnAddToMonitorList.Click += new System.EventHandler(this.btnAddToMonitorList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 112);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(510, 148);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnRun.Location = new System.Drawing.Point(15, 271);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(191, 31);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Start Monitoring";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // chkAutoStart
+            // 
+            this.chkAutoStart.AutoSize = true;
+            this.chkAutoStart.Location = new System.Drawing.Point(412, 279);
+            this.chkAutoStart.Name = "chkAutoStart";
+            this.chkAutoStart.Size = new System.Drawing.Size(113, 17);
+            this.chkAutoStart.TabIndex = 6;
+            this.chkAutoStart.Text = "Start Automatically";
+            this.chkAutoStart.UseVisualStyleBackColor = true;
+            this.chkAutoStart.CheckedChanged += new System.EventHandler(this.chkAutoStart_CheckedChanged);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Green;
+            this.lblStatus.Location = new System.Drawing.Point(30, 76);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(479, 24);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Auto Restart is Actively Monitoring these Programs";
+            this.lblStatus.Visible = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(212, 271);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(194, 32);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop Monitoring";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 315);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.chkAutoStart);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAddToMonitorList);
+            this.Controls.Add(this.btnGetFile);
+            this.Controls.Add(this.txtFileLoc);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Auto Restart";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileLoc;
+        private System.Windows.Forms.Button btnGetFile;
+        private System.Windows.Forms.Button btnAddToMonitorList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.CheckBox chkAutoStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using AutoRestart.Properties;
+using System.Threading;
+using System.Diagnostics;
+
+namespace AutoRestart
+{
+    public partial class Form1 : Form
+    {
+        
+        Thread th;
+
+        string titleCaption = "Auto Restart";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Look for data folder and load settings
+            // if data folder does NOT exist, create it.
+
+            string pathString = GetPathString();
+
+            if (Directory.Exists(pathString))
+            {
+                //read settings data
+                ReadSettingsFile();
+                chkAutoStart.Checked = Settings.Default.RunAutomatically;
+
+                if (chkAutoStart.Checked == true)
+                {
+                    // start the program monitor on programs inside the datagridview or data file
+                    StartMonitor();
+
+                }
+            }
+            else
+            {
+                CreateDataFolder(pathString);
+                CreateNewDataFile(pathString, "settings.txt");
+            }
+
+
+        }
+
+
+
+        private string GetPathString()
+        {
+            string folderName = Environment.GetEnvironmentVariable("localappdata"); ;
+            string pathString = System.IO.Path.Combine(folderName, "AutoRestartData");
+            return pathString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Environment.CurrentDirectory = Environment.GetEnvironmentVariable("programfiles");
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = String.Empty;
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtFileLoc.Text = openFileDialog1.FileName.ToString();
+        }
+
+        private void btnAddToMonitorList_Click(object sender, EventArgs e)
+        {
+            //Append data to file
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(GetPathString() + "\\settings.txt", true))
+            {
+                file.WriteLine(txtFileLoc.Text);
+            }
+
+            ReadSettingsFile();
+
+        }
+
+        private void CreateDataFolder(string path)
+        {
+            try
+            {
+
+                System.IO.Directory.CreateDirectory(path);
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Unable to create data folder", titleCaption);
+            }
+        }
+
+        private void CreateNewDataFile(string pathname, string filename)
+        {
+            string pathString = System.IO.Path.Combine(pathname, filename);
+
+            if (!System.IO.File.Exists(pathString))
+            {
+                System.IO.FileStream fs = System.IO.File.Create(pathString);
+                fs.Close();
+            }
+            else
+            {
+                MessageBox.Show("File ->" + filename + " already exists.", titleCaption);
+                return;
+            }
+
+        }
+
+        private void ReadSettingsFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(GetPathString() + "\\settings.txt");
+
+            List<KeyValuePair<int, string>> list = new List<KeyValuePair<int, string>>();
+
+            int l = lines.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+
+
+                list.Add(new KeyValuePair<int, string>(i, lines[i]));
+
+            }
+
+            dataGridView1.DataSource = list;
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            DialogResult choice = MessageBox.Show("Do you wish to remove this item from monitoring?", titleCaption, MessageBoxButtons.YesNo);
+
+
+            if (choice == DialogResult.Yes)
+            {
+
+                int row = e.RowIndex;
+
+                string[] lines = System.IO.File.ReadAllLines(GetPathString() + "\\settings.txt");
+
+                List<KeyValuePair<int, string>> list = new List<KeyValuePair<int, string>>();
+                int l = lines.Length;
+
+                for (int i = 0; i < l; i++)
+                {
+                    list.Add(new KeyValuePair<int, string>(i, lines[i]));
+                }
+
+
+                list.RemoveAt(row);
+
+                // clear previous file contents
+
+                System.IO.File.WriteAllBytes(GetPathString() + "\\settings.txt", new byte[0]);
+
+
+                //Append data to file
+
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(GetPathString() + "\\settings.txt", true))
+                {
+
+
+                    foreach (KeyValuePair<int, string> pair in list)
+                    {
+                        file.WriteLine(pair.Value);
+                    }
+
+                }
+
+                ReadSettingsFile();
+
+            }
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+            // Run monitor routines (timed interval to test if app is running
+
+            // System.Diagnostics.Process.Start ("location of the executable");
+
+            StartMonitor();
+
+
+        }
+
+        private void SaveSettings()
+        {
+
+            //Settings.Default.BaudRate = int.Parse(cmbBaudRate.Text);
+            Settings.Default.RunAutomatically = chkAutoStart.Checked;
+            Settings.Default.Save();
+
+        }
+
+        private void chkAutoStart_CheckedChanged(object sender, EventArgs e)
+        {
+
+            //if (chkAutoStart.Checked == true)
+            //    btnRun.Enabled = false;
+            //else
+            //    btnRun.Enabled = true;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (th != null)
+                th.Abort();
+            SaveSettings();
+        }
+
+        private void StartMonitor()
+        {
+            th = new Thread(TestApp); //test and make sure app is running  
+            th.Start();
+            lblStatus.Visible = true;
+            btnRun.Enabled = false;
+            dataGridView1.Enabled = false;
+
+        }
+
+        public void TestApp()
+        {
+            // if app is not running then Start app
+
+
+
+            string[] lines = System.IO.File.ReadAllLines(GetPathString() + "\\settings.txt");
+
+            Process[] processes;
+
+            int l = lines.Length;
+            while (th.IsAlive)
+            {
+                for (int i = 0; i < l; i++)
+                {
+
+                    processes = Process.GetProcessesByName(lines[i].Substring(lines[i].LastIndexOf(@"\") + 1, (lines[i].LastIndexOf(".") - lines[i].LastIndexOf(@"\")) - 1));
+                    if (processes.Length == 0)
+                        StartApp(lines[i].ToString());
+
+                }
+
+                System.Threading.Thread.Sleep(100);  // throttle cpu usage for other events .1 of a second
+            }
+        }
+
+        private void StartApp(string app)
+        {
+            string args = "";
+
+            if (app.IndexOf('/') > 0)
+            {
+                args = app.Substring(app.IndexOf('/'));
+                app = app.Replace(args, "");
+            }
+
+            
+            
+            System.Diagnostics.Process.Start(app, args);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (th != null)
+                th.Abort();
+            lblStatus.Visible = false;
+            btnRun.Enabled = true;
+            dataGridView1.Enabled = true;
+        }
+
+    }
+}
